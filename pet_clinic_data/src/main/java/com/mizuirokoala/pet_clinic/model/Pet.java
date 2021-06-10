a27bf0480680a2951b4df679b18491159aeaa36b
Damian Kamiński <damian.kaminski@euvic.pl>
2021-06-10 14:00:20 +0200

    base entity

--- pet_clinic_data/src/main/java/com/mizuirokoala/pet_clinic/model/Pet.java
+++ pet_clinic_data/src/main/java/com/mizuirokoala/pet_clinic/model/Pet.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
